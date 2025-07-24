bdc6894c71112e93771f561098e5f49f5d35ac0c
aahhmmeeddaammr <aamr24987@gmail.com>
2025-07-24 23:25:32 +0300

    FG

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import "./App.css";
-import Footer from "./components/footer/Footer";
-import NavBar from "./components/navbar/NavBar";
 import About from "./components/About/About";
 import Contact from "./components/contact/Contact";
 import Layout from "./components/LayOut/Layout";
@@ -24,7 +21,6 @@ let boda = createBrowserRouter([
 ]);
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
       <RouterProvider router={boda}></RouterProvider>

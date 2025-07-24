970c04a2c3bccd5b2188b1dde42164fa715be302
aahhmmeeddaammr <aamr24987@gmail.com>
2025-07-24 23:23:55 +0300

    F

---
+++ .history/src/components/LayOut/Layout_20250724191622.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../navbar/NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "../footer/Footer";
+import Footer from "./../Footer/Footer";
+
+export default function Layout() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
